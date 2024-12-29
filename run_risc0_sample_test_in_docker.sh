2a015e76694077be847d88c708780ab67d57fa4a
kaanselimkacar <kaan1410141@gmail.com>
2024-12-29 16:23:29 +0300

    minor: added helper bash scripts for easier testing of the docker environments

--- run_risc0_sample_test_in_docker.sh
+++ run_risc0_sample_test_in_docker.sh
@@ -1,1 +1,3 @@
-docker run --rm -v "$(pwd):/app" zkvm-benchmarking-risc0 make risc0 TEST_NAME=vecSum10
+#docker run --rm --privileged --ulimit nofile=65536:65536 -v "$(pwd):/app" zkvm-benchmarking-risc0 make risc0 TEST_NAME=fibonacci
+#docker run --rm --privileged --ulimit nofile=65536:65536 -v "$(pwd):/app" zkvm-benchmarking-risc0 make risc0 TEST_NAME=isprime
+docker run --rm --privileged --ulimit nofile=65536:65536 -v "$(pwd):/app" zkvm-benchmarking-risc0 make risc0 TEST_NAME=rsa
